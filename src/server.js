9d826492a6ab54b00632c850071e419420a2a35b
Vlad <vladut_stoian@live.co.uk>
2022-08-30 11:01:46 +0100

    Made it run the web server via path

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000
 
+app.use(express.static("public"))
+
 app.listen(port, () =>
 {
-    console.log("listening on port 5000")
+    console.log(`listening on port ${port}`)
 })
